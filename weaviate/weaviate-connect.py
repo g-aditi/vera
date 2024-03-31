cd5ba601d2e42816f4f8e0952311bd7ad1448f27
Vihang26 <vhpancholi@gmail.com>
2024-03-31 14:54:26 -0700

    weaviate client example

---
+++ weaviate/weaviate-connect.py
@@ -0,0 +1,41 @@
+import weaviate
+import weaviate.classes as wvc
+import os
+import requests
+import json
+
+client = weaviate.connect_to_wcs(
+    cluster_url=os.getenv("WCS_CLUSTER_URL"),
+    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_API_KEY")),
+    headers={
+        "X-OpenAI-Api-Key": os.environ["OPENAI_APIKEY"]  # Replace with your inference API key
+    }
+)
+
+try:
+    pass # Replace with your code. Close client gracefully in the finally block.
+    # ===== define collection =====
+    questions = client.collections.create(
+        name="Question",
+        vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_openai(),  # If set to "none" you must always provide vectors yourself. Could be any other "text2vec-*" also.
+        generative_config=wvc.config.Configure.Generative.openai()  # Ensure the `generative-openai` module is used for generative queries
+    )
+
+    # ===== import data =====
+    resp = requests.get('https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json')
+    data = json.loads(resp.text)  # Load data
+
+    question_objs = list()
+    for i, d in enumerate(data):
+        question_objs.append({
+            "answer": d["Answer"],
+            "question": d["Question"],
+            "category": d["Category"],
+        })
+
+    questions = client.collections.get("Question")
+    questions.data.insert_many(question_objs)  # This uses batching under the hood
+
+
+finally:
+    client.close()  # Close client gracefully
